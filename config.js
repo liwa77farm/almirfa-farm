ba702b620f7d70fb3d00cb4ca2bff5ba855d120d
liwa77farm <salem123ssmi@gmail.com>
2026-07-31 17:50:24 +0400

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ window.ALMIRFA_CONFIG = {
   farmId: 'almirfa',
   accessPin: '7788',
   supabaseUrl: 'https://lzniczlruexijlnxoaah.supabase.co',
-  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhwZGJwbmFiY2xrY3VjbHh3dXdlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUyNTU1NTIsImV4cCI6MjEwMDgzMTU1Mn0.mgapSzAuHT07S0w8YYr39ht3XBOmTv2G4PHfbRAJYz0'
+  supabaseAnonKey: 'sb_publishable_EWrp4UumHdst-SsxAbW5ew_E3fVAV1V'
 };
